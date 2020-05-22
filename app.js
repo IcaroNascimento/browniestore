fb8612291174921c8033a03bdbbe631b86bc3fa3
Icaro Nascimento <icaronascimento1@gmail.com>
2020-05-22 06:40:24 -0300

    feature: implement routes module

--- app.js
+++ app.js
@@ -1,17 +1,15 @@
 const express = require('express');
-// load env variables
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 dotenv.config();
 
+const userRoutes = require('./routes/user');
 const app = express();
 
-// import mongoose
-const mongoose = require('mongoose');
-
-//db connection
 mongoose
 	.connect(process.env.MONGO_URI, {
 		useNewUrlParser: true,
+		useCreateIndex: true,
 		useUnifiedTopology: true
 	})
 	.then(() => console.log('DB Connected'));
@@ -20,16 +18,13 @@ mongoose.connection.on('error', (err) => {
 	console.log(`DB connection error: ${err.message}`);
 });
 
-//routes
-app.get('/', (request, response) => {
-	response.send('Hello from node');
-});
+app.use('/api',userRoutes);
 
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
 	console.log(
 		`Server is running on port ${port} 
-Click here http://localhost:8000 or put this url in your browser`
+Click here http://localhost:8000/api to entry point or put this url in your browser`
 	);
 });
