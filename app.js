3670bd3374e07ef73b7bf325d99d8a88a3cc49a7
Icaro Nascimento <icaronascimento1@gmail.com>
2020-05-22 04:16:48 -0300

    feature: implement MongoDB connection with mongoose

--- app.js
+++ app.js
@@ -1,8 +1,26 @@
 const express = require('express');
+// load env variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = express();
 
-require('dotenv').config();
+// import mongoose
+const mongoose = require('mongoose');
 
+//db connection
+mongoose
+	.connect(process.env.MONGO_URI, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true
+	})
+	.then(() => console.log('DB Connected'));
+
+mongoose.connection.on('error', (err) => {
+	console.log(`DB connection error: ${err.message}`);
+});
+
+//routes
 app.get('/', (request, response) => {
 	response.send('Hello from node');
 });
@@ -11,7 +29,7 @@ const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
 	console.log(
-    `Server is running on port ${port} 
+		`Server is running on port ${port} 
 Click here http://localhost:8000 or put this url in your browser`
-  );
+	);
 });
