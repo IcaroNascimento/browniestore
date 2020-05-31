e6c2adaf33402afbc4ff8e504ecfc1a31bef8ccb
Icaro Nascimento <icaronascimento1@gmail.com>
2020-05-31 16:56:58 -0300

    feature: create validations to fields in product controller

--- controllers/product.js
+++ controllers/product.js
@@ -13,9 +13,23 @@ exports.create = (request, response) => {
 				error: 'Image could not be uploaded'
 			});
 		}
+
+		const { name, description, price, quantity, photo, shipping } = fields;
+
+		if (!name || !description || !price || !quantity || !photo || !shipping) {
+			return response.status(400).json({
+				error: 'All fields are required'
+			});
+		}
+
 		let product = new Product(fields);
 
 		if (files.photo) {
+			if (files.photo.size > 1000000) {
+				return response.status(400).json({
+					error: 'Image should be less than 1mb in size'
+				});
+			}
 			product.photo.data = fs.readFileSync(files.photo.path);
 			product.photo.contentType = files.photo.type;
 		}
