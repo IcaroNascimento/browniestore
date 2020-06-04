c4e29d8098de07ba7299a39dc5a50240fcfe0367
Icaro Nascimento <icaronascimento1@gmail.com>
2020-06-03 21:16:15 -0300

    feature: read and update user

--- controllers/user.js
+++ controllers/user.js
@@ -1,14 +1,32 @@
 const User = require('../models/user');
 
-exports.userById = (request, response, next, id) => {   
+exports.userById = (request, response, next, id) => {
 	User.findById(id).exec((error, user) => {
-        if (error || !user) {
+		if (error || !user) {
 			return response.status(400).json({
 				error: 'User not found'
 			});
 		}
-        
+
 		request.profile = user;
-        next();
+		next();
+	});
+};
+exports.read = (request, response) => {
+	request.profile.hashed_password = undefined;
+	request.profile.salt = undefined;
+	return response.json(request.profile);
+};
+
+exports.update = (request, response) => {
+	User.findOneAndUpdate({ _id: request.profile._id }, { $set: request.body }, { new: true }, (error, user) => {
+		if (error) {
+			return response.status(400).json({
+				error: 'You are not authorized to perform this action'
+			});
+		}
+		user.hashed_password = undefined;
+		user.salt = undefined;
+		response.json(user);
 	});
 };
--- routes/user.js
+++ routes/user.js
@@ -3,13 +3,16 @@ const router = express.Router();
 
 const { requireSignin, isAuth, isAdmin } = require('../controllers/auth');
 
-const { userById } = require('../controllers/user');
+const { userById, read, update } = require('../controllers/user');
 
 router.get('/secret/:userId', requireSignin, isAuth, isAdmin, (request, response) =>{
     response.json({
         user: request.profile
     })
 });
+
+router.get('/user/:userId', requireSignin, isAuth, read)
+router.put('/user/:userId', requireSignin, isAuth, update)
 
 router.param('userId', userById);
 
