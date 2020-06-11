4358834160cd0c74c729fcef4a7454093acfa76d
Icaro Nascimento <icaronascimento1@gmail.com>
2020-06-11 17:27:52 -0300

    feature: include close button to modal

--- browniestore-front/src/MyModal.js
+++ browniestore-front/src/MyModal.js
@@ -1,11 +1,11 @@
 import React, { useState } from 'react';
-import { Modal, makeStyles } from '@material-ui/core';
-import Icon from '@material-ui/core/Icon';
+import { Modal, makeStyles, Icon } from '@material-ui/core';
 import './core/Menu.css';
 import SigninForm from './SigninForm';
+
 function getModalStyle() {
-	const top = 50 
-	const left = 50 
+	const top = 50;
+	const left = 50;
 
 	return {
 		top: `${top}%`,
@@ -18,13 +18,22 @@ const useStyles = makeStyles((theme) => ({
 	paper: {
 		display: 'flex',
 		alignItems: 'center',
-		justifyContent:'center',
+		justifyContent: 'center',
 		position: 'absolute',
 		width: 500,
 		height: 300,
 		backgroundColor: theme.palette.background.paper,
 		boxShadow: theme.shadows[5],
-		padding: theme.spacing(1, 1, 1),
+		padding: theme.spacing(1, 1, 1)
+	},
+	closeIcon: {
+		top: `3%`,
+		left: `2%`,
+		position: 'absolute',
+		color: '#000000',
+		'&:hover': {
+			color: '#707070'
+		}
 	}
 }));
 
@@ -44,20 +53,19 @@ export default function SimpleModal() {
 
 	const body = (
 		<div style={modalStyle} className={classes.paper}>
+			<Icon onClick={handleClose} className={classes.closeIcon}>
+				clear
+			</Icon>
 			<SigninForm />
 		</div>
 	);
 
 	return (
 		<div>
-			<Icon onClick={handleOpen} className="icons">person</Icon>
-			<Modal
-				open={open}
-				onClose={handleClose}
-				aria-labelledby="simple-modal-title"
-			>
-				{body}
-			</Modal>
+			<Icon onClick={handleOpen} className="icons">
+				person
+			</Icon>
+			<Modal open={open}>{body}</Modal>
 		</div>
 	);
 }
