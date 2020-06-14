021b8c505dca686ecd45f083feb9c1879dd1f7f3
Icaro Nascimento <icaronascimento1@gmail.com>
2020-06-14 17:28:34 -0300

    feature: get data values from SignupForm

--- browniestore-front/src/SignupForm.js
+++ browniestore-front/src/SignupForm.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { TextField, Button, makeStyles } from '@material-ui/core';
 
 import SigninModal from './SigninModal';
@@ -25,27 +25,43 @@ const useStyles = makeStyles((theme) => ({
 		display: 'inline-block',
 		fontSize: 12,
 		clear: 'both',
-    	whiteSpace: 'nowrap'
-
-    }
+		whiteSpace: 'nowrap'
+	}
 }));
 
 export default function SignupForm() {
 	const classes = useStyles();
 
+	const [ values, setValues ] = useState({
+		name: '',
+		email: '',
+		password: '',
+		error: '',
+		success: false
+	});
+
+	const handleChange = (name) => (event) => {
+		setValues({ ...values, error: false, [name]: event.target.value });
+	};
+
 	return (
 		<form className={classes.root} noValidate autoComplete="on">
 			<div>Crie sua conta</div>
-			<TextField id="standard-basic" label="Nome completo" />
-			<TextField id="standard-basic" label="Email" />
-			<TextField id="standard-basic" label="Escolha sua senha" type="password" />
-			<TextField id="standard-basic" label="Repita sua senha" type="password" />
+			<TextField onChange={handleChange('name')} id="standard-basic" label="Nome completo" />
+			<TextField onChange={handleChange('email')} id="standard-basic" label="Email" />
+			<TextField
+				onChange={handleChange('password')}
+				id="standard-basic"
+				label="Escolha sua senha"
+				type="password"
+			/>
 			<Button className={classes.button} variant="contained">
 				Criar conta
 			</Button>
 			<div className={classes.link}>
 				<SigninModal />
 			</div>
+			{JSON.stringify(values)}
 		</form>
 	);
 }
